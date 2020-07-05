d4fa10a0c620ed647294b8edcd0cf05197278e8e
carsonetb <63245844+carsonetb@users.noreply.github.com>
2020-07-05 13:52:28 -0700

    Create SpaceArena.py

---
+++ SpaceArena.py
@@ -0,0 +1,30 @@
+# Space Arena
+# The Ultimate Game
+# Python 3.x Compatible
+# Windows, MacOSX, and Linux Compatible
+# By Carson E.T Bates
+
+import turtle
+
+SCREEN_WIDHT = 800
+SCREEN_HEIGHT = 600
+
+wn = turtle.Screen()
+wn.setup(SCREEN_WIDHT, SCREEN_HEIGHT)
+wn.title("                                                                                               Space Arena                                                                                     ")
+wn.bgcolor("black")
+
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+
+
+
+
+
+
+
+wn.mainloop()
